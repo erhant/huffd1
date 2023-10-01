b3cb29681f7525e671057dbbc08a2b3237bd2f69
erhant <erhany96@gmail.com>
2023-10-01 11:28:50 +0300

    eval ownership works, sload and sstore poly

--- test/Huffd1.t.sol
+++ test/Huffd1.t.sol
@@ -8,6 +8,7 @@ import {console} from "forge-std/console.sol";
 contract Huffd1Test is Test {
     Huffd1 huffd1;
     address constant OWNER = address(0x1); // small address to work for all orders
+    uint256 constant TOTAL_SUPPLY = 10;
 
     /// @dev Set-up to run before each test.
     function setUp() public {
@@ -22,13 +23,19 @@ contract Huffd1Test is Test {
     }
 
     /// @dev Should have the correct owner.
-    function test_Owned() public {
+    function test_ContractOwnership() public {
         assertEq(OWNER, huffd1.owner());
 
-        address NEW_OWNER = address(0x2);
+        address NEW_OWNER = address(0x9);
         vm.prank(OWNER);
         huffd1.setOwner(NEW_OWNER);
         assertEq(NEW_OWNER, huffd1.owner());
+    }
+
+    /// @dev Should own tokens at the start.
+    function test_Ownership() public {
+        assertEq(OWNER, huffd1.ownerOf(0));
+        assertEq(OWNER, huffd1.ownerOf(1));
     }
 }
 
@@ -43,4 +50,12 @@ interface Huffd1 is Owned {
     function name() external view returns (string memory name);
 
     function symbol() external view returns (string memory symbol);
+
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    function transfer(address to, uint256 tokenId) external;
+
+    function balanceOf(address owner) external returns (uint256 balance);
+
+    // TODO: approvals?
 }
--- test/Polynomial.t.sol
+++ test/Polynomial.t.sol
@@ -6,42 +6,63 @@ import {Test} from "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 contract PolynomialTest is Test {
-    IPolynomial public polynomial;
+    IPolynomial polynomial;
+    uint256 constant TOTAL_SUPPLY = 3;
+    uint256 constant ORDER = 13;
+    bool constant VERBOSE = false;
 
     function setUp() public {
         string memory code = vm.readFile("test/mocks/PolynomialWrappers.huff");
         polynomial = IPolynomial(HuffDeployer.deploy_with_code("util/Polynomial", code));
     }
 
-    function test_Eval() public view {
-        // uint256[3] memory result = polynomial.eval(0);
-        // console.log(result[0], result[1], result[2]);
-        console.log("");
-        console.log("L0(0): ", polynomial.eval(0, 0));
-        console.log("L0(1): ", polynomial.eval(0, 1));
-        console.log("L0(2): ", polynomial.eval(0, 2));
-
-        console.log("");
-        console.log("L1(0): ", polynomial.eval(1, 0));
-        console.log("L1(1): ", polynomial.eval(1, 1));
-        console.log("L1(2): ", polynomial.eval(1, 2));
-
-        console.log("");
-        console.log("L2(0): ", polynomial.eval(2, 0));
-        console.log("L2(1): ", polynomial.eval(2, 1));
-        console.log("L2(2): ", polynomial.eval(2, 2));
+    function test_Eval() public {
+        for (uint256 basis = 0; basis < TOTAL_SUPPLY; ++basis) {
+            VERBOSE ? console.log("") : ();
+            for (uint256 tokenId = 0; tokenId < TOTAL_SUPPLY; ++tokenId) {
+                uint256 eval = polynomial.eval(basis, tokenId);
+                assertEq(eval, basis == tokenId ? 1 : 0);
+                VERBOSE ? console.log("BASIS(", basis, "):", eval) : ();
+            }
+        }
     }
 
+    // TODO: do more exhaustive tests
     function test_AddEval() public view {
         console.log("L0(0) + L0(0): ", polynomial.addEval(0, 0, 0));
         console.log("L0(0) + L1(0): ", polynomial.addEval(0, 1, 0));
         console.log("L0(0) + L2(0): ", polynomial.addEval(0, 2, 0));
     }
 
-    function test_ScaleEval() public view {
-        console.log("L0(0) * 0: ", polynomial.scaleEval(0, 0, 0));
-        console.log("L0(0) * 1: ", polynomial.scaleEval(0, 1, 0));
-        console.log("L0(0) * 2: ", polynomial.scaleEval(0, 2, 0));
+    function test_ScaleEval() public {
+        uint256 scale = ORDER / 2; // arbitrary
+        for (uint256 basis = 0; basis < TOTAL_SUPPLY; ++basis) {
+            VERBOSE ? console.log("") : ();
+            for (uint256 tokenId = 0; tokenId < TOTAL_SUPPLY; ++tokenId) {
+                uint256 eval = polynomial.eval(basis, tokenId);
+                uint256 scaledEval = polynomial.scaleEval(basis, scale, tokenId);
+                assertEq(scaledEval, basis == tokenId ? (eval * scale) % ORDER : 0);
+                VERBOSE ? console.log("BASIS(0) *", scale, "=", scaledEval) : ();
+            }
+        }
+    }
+
+    function test_AddScaleStore() public {
+        console.log("P(0)", polynomial.evalStore(0));
+        console.log("P(1)", polynomial.evalStore(1));
+        console.log("P(2)", polynomial.evalStore(2));
+
+        polynomial.addStore(1);
+
+        console.log("P(0) + L1(0)", polynomial.evalStore(0));
+        console.log("P(1) + L1(1)", polynomial.evalStore(1));
+        console.log("P(2) + L1(2)", polynomial.evalStore(2));
+
+        polynomial.scaleStore(5);
+
+        console.log("5(P(0) + L1(0))", polynomial.evalStore(0));
+        console.log("5(P(1) + L1(1))", polynomial.evalStore(1));
+        console.log("5(P(2) + L1(2))", polynomial.evalStore(2));
     }
 }
 
@@ -51,4 +72,10 @@ interface IPolynomial {
     function addEval(uint256 basis1, uint256 basis2, uint256 point) external view returns (uint256 evaluation);
 
     function scaleEval(uint256 basis, uint256 scale, uint256 point) external view returns (uint256 evaluation);
+
+    function evalStore(uint256 point) external view returns (uint256 evaluation);
+
+    function addStore(uint256 basis) external;
+
+    function scaleStore(uint256 scale) external;
 }
